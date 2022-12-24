183a0af1ac6ddab905eb7839738008c7696ebdcc
Bishalstha35 <117627174+Bishalstha35@users.noreply.github.com>
2022-12-24 15:52:12 +0545

    Final Commit

---
+++ display.php
@@ -0,0 +1,63 @@
+<?php
+include 'connect.php';
+?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>student_info</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css">
+</head>
+<body>
+    <div class="container">
+        <button class="btn btn-primary my-5"><a href="student_info.php"class="text-light">Add user</a></button>
+        <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">Sl no</th>
+      <th scope="col">Name</th>
+      <th scope="col">Email</th>
+      <th scope="col">Mobile</th>
+      <th scope="col">Rollno</th>
+      <th scope="col">Operations</th>
+    </tr>
+  </thead>
+  <tbody>
+
+
+  <?php
+$sql="Select * from student";
+$result=mysqli_query($con,$sql);
+if($result){
+    while($row=mysqli_fetch_assoc($result)){
+        $id=$row['id'];
+        $name=$row['name'];
+        $email=$row['email'];
+        $mobile=$row['mobile'];
+        $rollno=$row['rollno'];
+        echo '<tr>
+        <th scope="row">'.$id.'</th>
+        <td>'.$name.'</td>
+        <td>'.$email.'</td>
+        <td>'.$mobile.'</td>
+        <td>'.$rollno.'</td>
+        <td>
+        <button class="btn btn-primary"><a href="update.php?updateid='.$id.'"class="text-light">Update</a></button>
+        <button class="btn btn-danger"><a href="delete.php?deleteid='.$id.'" class="text-light">Delete</a></button>
+    </td>
+      </tr>';
+    }
+
+}
+
+
+?>
+  
+  </tbody>
+</table>
+</div>
+</body>
+</html>
